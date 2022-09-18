c357e7f1b5408e4ec7403453db209287bd9bc96e
helz <helenaristanovic3@gmail.com>
2022-09-18 09:20:02 -0400

    Added BG Music

---
+++ Assets/scripts/MusicControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour
+{
+    public static MusicControl instance; //creates a static variable for the Music Control instance
+
+    private void Awake() //runs before Start()
+    {
+        DontDestroyOnLoad(this.gameObject); //prevents from destroying Game Object in diff scenes
+
+        if(instance == null) //If the Music Control instance variable is null
+        {
+            instance = this; //set this object as the instance
+        }
+        else //if there is already a Music Control instance active
+        {
+            Destroy(gameObject);
+        }
+    }
+}
